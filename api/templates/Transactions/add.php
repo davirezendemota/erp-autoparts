39f68ec60432968f5b5ce48c885ff025695afdc4
Rafael <rafaelduarte231q@gmail.com>
2025-08-08 20:55:34 -0300

    feat(view): cria views para CRUD de transactions

---
+++ api/templates/Transactions/add.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Transaction $transaction
+ * @var \Cake\Collection\CollectionInterface|string[] $items
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('List Transactions'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column column-80">
+        <div class="transactions form content">
+            <?= $this->Form->create($transaction) ?>
+            <fieldset>
+                <legend><?= __('Add Transaction') ?></legend>
+                <?php
+                    echo $this->Form->control('item_id', ['options' => $items]);
+                    echo $this->Form->control('tipo');
+                    echo $this->Form->control('data_transacao');
+                    echo $this->Form->control('quantidade');
+                    echo $this->Form->control('preco_total');
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
+</div>
