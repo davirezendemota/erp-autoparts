dae119f2d1e9ba4fb7d473927c0abb2c91adb9a6
Rafael <rafaelduarte231q@gmail.com>
2025-08-17 19:19:34 -0300

    feat(view): Adiciona conexão do html/homephp com css

--- api/templates/Pages/home.php
+++ api/templates/Pages/home.php
@@ -1,10 +1,10 @@
 <?php
 $this->assign('title', 'Pagina inicial')
 ?>
-
+<!-- Importa Bootstrap e home.css -->
 <?= $this->Html->css('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css') ?>
-
-
+<?= $this->Html->css('home') ?>
+ 
 <div class="container d-flex justify-content-center align-items-center" style="min-height: 40vh;">
     <div class="text-center">
         <h1 class="mb-5">Controle de Estoque</h1>
